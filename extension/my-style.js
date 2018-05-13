d70c21d6b1c28ef1fba8a98ff001ad71d6dc5f36
Dang <huynhminhdang@gmail.com>
2018-05-13 08:32:55 +0800

    refactor: rewrite

--- extension/my-style.js
+++ extension/my-style.js
@@ -6,10 +6,10 @@
   const SOFT_TAB = '    ';
   const SOFT_TAB_LENGTH = SOFT_TAB.length;
 
-  const ONLY_WHITESPACE_REGEX = /^\s*$/;
-  const WHITESPACE_SPLIT_REGEX = /\s+$/g;
+  const STORAGE_KEY = 'mystyle_' + window.location.hostname;
 
-  /* Throttle the given function, condensing multiple calls into one call after
+  /**
+   * Throttle the given function, condensing multiple calls into one call after
    * the given timeout period. In other words, allow at most one call to go
    * through per timeout period. Returns the throttled function.
    *
@@ -31,153 +31,182 @@
     };
   }
 
-  window.addEventListener('DOMContentLoaded', event => {
-    let key = 'mystyle_' + window.location.hostname;
-    chrome.storage.sync.get(key, my_css => {
-      console.log(JSON.stringify(my_css));
-
-      let head = document.getElementsByTagName('head')[0];
-      let body = document.body;
-
-      let style = document.createElement('style');
-      let textarea = document.createElement('textarea');
-
-      // hide textarea by default
-      textarea.style.display = 'none';
-      textarea.id = 'my-style-input';
-      textarea.spellcheck = false;
-
-      head.appendChild(style);
-      body.appendChild(textarea);
-
-      style.innerHTML = my_css[key] || localStorage.myStyle || '';
-      textarea.value = style.innerHTML;
-      textarea.placeholder = '/* Enter your styles here. */';
-
-      // alt + click on an element adds its selector to the textarea
-      body.addEventListener('click', event => {
-        // ensure textarea is actually displayed
-        if (
-          textarea.style.display.indexOf('none') === -1 &&
-          event.target.id !== textarea.id &&
-          event.altKey
-        ) {
-          let i = 0;
-          let target = event.target;
-          let elemClass = target.className.split(' ') || '';
-          let stylesList = [];
-          let existingStyles = '';
-          let selector = '';
-          let cssStatement;
-          let textToAdd;
-
-          // selector starts with the tag
-          selector += target.tagName.toLowerCase();
-
-          // include ID if there is one
-          if (target.id) {
-            selector += '#' + target.id;
-          }
-
-          // include all classes found
-          for (i = 0; i < elemClass.length; i++) {
-            if (!ONLY_WHITESPACE_REGEX.test(elemClass[i])) {
-              selector += '.' + elemClass[i];
-            }
-          }
-
-          // fill CSS with styles defined in the style attribute
-          if (target.getAttribute('style')) {
-            stylesList = target.getAttribute('style').split(';');
-
-            // keep track of CSS properties already defined in style attribute
-            for (i = 0; i < stylesList.length; i++) {
-              // condense mutliple whitespace into one space
-              cssStatement = stylesList[i]
-                .split(WHITESPACE_SPLIT_REGEX)
-                .join(' ')
-                .trim();
-
-              if (!ONLY_WHITESPACE_REGEX.test(cssStatement)) {
-                existingStyles += SOFT_TAB + cssStatement.toLowerCase() + ';\n';
-              }
-            }
-          }
-
-          // construct text to add to textarea
-          if (selector) {
-            // add existing styles in braces
-            if (existingStyles) {
-              existingStyles = '{\n' + existingStyles + '}';
-            } else {
-              existingStyles = '{\n\n}';
-            }
-
-            textToAdd = '\n' + selector + ' ' + existingStyles;
-            textarea.value += textToAdd;
-
-            // highlight added text for easy removal
-            textarea.focus();
-            textarea.setSelectionRange(
-              textarea.value.length - textToAdd.length,
-              textarea.value.length
-            );
-          }
-
-          event.preventDefault();
-        }
+  function loadStyles(key /*: string */ = STORAGE_KEY) /*: Promise<string> */ {
+    if (!loadStyles.cached) {
+      loadStyles.cached = new Promise(resolve => {
+        chrome.storage.sync.get(key, result => {
+          resolve(result[key] || '');
+        });
       });
+    }
 
-      /* Save styles persistently in local storage. */
-      let saveStyles = throttle(() => {
-        localStorage.myStyle = style.innerHTML;
-        let key = 'mystyle_' + window.location.hostname;
-        let obj = {};
-        obj[key] = style.innerHTML;
-        chrome.storage.sync.set(obj);
+    return loadStyles.cached;
+  }
+
+  /**
+   * Save styles persistently in local storage.
+   */
+  function saveStyles(css /*: string */, key /*: string */ = STORAGE_KEY) {
+    if (!saveStyles.throttledFn) {
+      saveStyles.throttledFn = throttle(css => {
+        chrome.storage.sync.set({ [key]: css });
       }, 500);
+    }
 
-      /* Updates styles with content in textarea and saves styles. */
-      function updateAndSaveStyles() {
-        style.innerHTML = textarea.value;
-        saveStyles();
+    saveStyles.throttledFn(css);
+  }
+
+  function applyStyles(css /*: string */ = '') {
+    if (!applyStyles.$style) {
+      let $style = document.createElement('style');
+      document.head.appendChild($style);
+      applyStyles.$style = $style;
+    }
+
+    applyStyles.$style.innerHTML = css;
+  }
+
+  /**
+   * Get element selector as tag#id.class1.class2
+   */
+  function generateCSSSelector(element /*: HTMLElement */) /*: string */ {
+    let selector = '';
+
+    // selector starts with the tag
+    selector += element.tagName.toLowerCase();
+
+    // include ID if there is one
+    if (element.id) {
+      selector += '#' + element.id;
+    }
+
+    // include all classes found
+    if (element.classList.length) {
+      selector += '.' + [...element.classList.values()].join('.');
+    }
+
+    return selector;
+  }
+
+  function getInlineStyles(
+    element /*: HTMLElement */,
+    indent /*: string */ = SOFT_TAB
+  ) /*: string */ {
+    if (element.styles == null) return '';
+
+    let rules = element.styles.value.split(';').filter(Boolean);
+
+    return rules.map(x => indent + x.trim()).join('\n');
+  }
+
+  function buildTextAreaEditor(
+    css /*: string */,
+    onChange /*: Function */
+  ) /*: HTMLElement */ {
+    let $textarea = document.createElement('textarea');
+
+    $textarea.id = 'my-style-input';
+    $textarea.spellcheck = false;
+    $textarea.value = css;
+    $textarea.placeholder = `/* Enter your styles here. */
+
+/* Alt + Click on an element to add it to the editor. */`;
+
+    // alt + click on an element adds its selector to the textarea
+    document.body.addEventListener('click', event => {
+      // do nothing when editor is off
+      if ($textarea.style.display !== 'block') return;
+
+      // do nothing if click on the editor
+      if ($textarea.isSameNode(event.target)) return;
+
+      // only handle Alt + Click
+      if (!event.altKey) return;
+      if (event.ctrlKey) return;
+      if (event.shiftKey) return;
+      if (event.metaKey) return;
+
+      event.preventDefault();
+
+      // construct text to add to textarea
+      let selector = generateCSSSelector(event.target);
+      if (selector) {
+        let existingStyles = getInlineStyles(event.target);
+        let textToAdd = `${selector} {\n${existingStyles || SOFT_TAB}\n}`;
+
+        $textarea.value = $textarea.value.trimRight() + '\n\n' + textToAdd;
+
+        // highlight added text for easy removal
+        $textarea.focus();
+        $textarea.setSelectionRange(
+          $textarea.value.length - textToAdd.length,
+          $textarea.value.length
+        );
       }
+    });
 
-      // continually update styles with textarea content
-      textarea.addEventListener('keyup', updateAndSaveStyles);
-      textarea.addEventListener('change', updateAndSaveStyles);
+    // continually update styles with textarea content
+    $textarea.addEventListener('keyup', () => onChange($textarea.value));
+    $textarea.addEventListener('change', () => onChange($textarea.value));
 
-      // pressing tab should insert spaces instead of focusing another element
-      textarea.addEventListener('keydown', event => {
-        let value = textarea.value;
-        let caret = textarea.selectionStart;
+    // pressing tab should insert spaces instead of focusing another element
+    $textarea.addEventListener('keydown', event => {
+      let value = $textarea.value;
+      let caret = $textarea.selectionStart;
 
-        // if tab is pressed, insert four spaces
-        if (event.keyCode === TAB_KEY_CODE) {
-          textarea.value =
-            value.substring(0, caret) + SOFT_TAB + value.substring(caret);
+      // if tab is pressed, insert four spaces
+      if (event.keyCode === TAB_KEY_CODE) {
+        $textarea.value =
+          value.substring(0, caret) + SOFT_TAB + value.substring(caret);
 
-          // move caret to after soft tab
-          textarea.setSelectionRange(
-            caret + SOFT_TAB_LENGTH,
-            caret + SOFT_TAB_LENGTH
-          );
+        // move caret to after soft tab
+        $textarea.setSelectionRange(
+          caret + SOFT_TAB_LENGTH,
+          caret + SOFT_TAB_LENGTH
+        );
 
-          // prevent default tab action that shifts focus to the next element
-          event.preventDefault();
+        // prevent default tab action that shifts focus to the next element
+        event.preventDefault();
+      }
+    });
+
+    return $textarea;
+  }
+
+  /* Preload styles */
+  loadStyles().then(css => {
+    console.group('my-style');
+    console.log(css);
+    console.groupEnd();
+  });
+
+  window.addEventListener('DOMContentLoaded', async event => {
+    let css = await loadStyles();
+
+    applyStyles(css);
+
+    let $editor = buildTextAreaEditor(css, css => {
+      /* Updates styles with content in textarea and saves styles. */
+      applyStyles(css);
+      saveStyles(css);
+    });
+
+    // hide editor by default
+    $editor.style.display = 'none';
+    document.body.appendChild($editor);
+
+    // control + m toggles text area
+    window.addEventListener('keydown', event => {
+      if (event.ctrlKey && event.keyCode === M_KEY_CODE) {
+        if ($editor.style.display !== 'block') {
+          $editor.style.display = 'block';
+          this.lastFocusElement = document.activeElement;
+          $editor.focus();
+        } else {
+          $editor.style.display = 'none';
+          this.lastFocusElement && this.lastFocusElement.focus();
         }
-      });
-
-      window.addEventListener('keydown', event => {
-        // control + m toggles text area
-        if (event.ctrlKey && event.keyCode === M_KEY_CODE) {
-          if (textarea.style.display == 'none') {
-            textarea.style.display = 'block';
-          } else {
-            textarea.style.display = 'none';
-          }
-        }
-      });
+      }
     });
   });
 })();
